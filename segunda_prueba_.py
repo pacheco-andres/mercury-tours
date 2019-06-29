900f4aae3c9dcfc113a4ed31e2aa0c2c4900df96
Andres Pacheco <andres.pacheco@vetta.io>
2019-06-28 23:56:02 -0500

    nuevos cambios del proyecto

--- segunda_prueba_.py
+++ segunda_prueba_.py
@@ -10,5 +10,11 @@ driver.find_element_by_xpath("//tr[5]/td/form/table/tbody/tr[10]/td[2]/input").s
 driver.find_element_by_xpath("//tr[11]/td[2]/input").send_keys("123456")
 driver.find_element_by_xpath("//select/option[3]").click()   
 time.sleep(3)
+driver.find_element_by_xpath("//*[@id='email']").send_keys("andres.pacheco.io")
+driver.find_element_by_xpath("//tr[15]/td[2]/input").send_keys("vetta.andres")
+driver.find_element_by_xpath("//tr[16]/td[2]/input").send_keys("vetta.andres")
+driver.find_element_by_name("register").click()
+time.sleep(3)
+
 
 driver.quit()
